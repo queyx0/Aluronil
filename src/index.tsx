6594e9f2263321041eeb84dcf2ff0f49d8bd0633
Antonio Eduardo Garcia Moreira <tonyeg2020@gmail.com>
2023-01-27 10:01:30 -0300

    class 2 complete, import svg, asolute imports. svg as component, reset css

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import Cardapio from "./Cardapio";
 import "./index.css";
 
 const root = ReactDOM.createRoot(
@@ -7,6 +8,6 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <div> Aluronil </div>
+    <Cardapio />
   </React.StrictMode>
 );
